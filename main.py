4d83bd9079d530407840209608ac8c61dd7ad56c
kevin-krause <contatokevinkrause@gmail.com>
2023-04-28 10:56:52 -0300

    main updt

--- main.py
+++ main.py
@@ -7,56 +7,17 @@ from config import prod
 def getEstoque(query):
     conn = pyodbc.connect(prod)
 
-    sql = pd.read_sql(query, conn)
+    cursor = conn.cursor().execute(query)
+    columns = [column[0] for column in cursor.description]
+    results = []
+    for row in cursor.fetchall():
+        results.append(dict(zip(columns, row)))
 
-    df = pd.DataFrame(sql, columns=['EMPRESA', 'REVENDA', 'ANOMES', 'DAATA', 'ITEM_ESTOQUE', 'ITEM_ESTOQUE_PUB', 'DES_ITEM_ESTOQUE', 'MARCA', 'GRUPO', 'CATEGORIA', 'DES_CATEGORIA', 'IDENTIFICACAO', 'UTILIZACAO_ITEM', 'DIAS_SEM_MOVIMENTO', 'DIAS_MOVIMENTO_INICIAL', 'DTA_ULT_ENTRADA', 'VAL_ESTOQUE', 'QTD_CONTABIL', 'CUSTO_MEDIO', 'QTD_PEDIDA', 'QTD_RES_OFICINA', 'QTD_NEGOCIACAO',
-                      'QTD_ALOCADA', 'QTD_ORCADA', 'QTD_LITIGIO', 'QTD_TRANSITO', 'QTD_TERCEIROS', 'ORDEM_TRANSACAO', 'BASE_ICMS_RETIDO', 'QTD_CONFERENCIA', 'QTD_COMPROMETIDA', 'PRECO_CUSTO', 'PRECO_PUBLICO', 'VAL_ESTOQUE_SEM_ICMSRET', 'VAL_ICMS_RETIDO', 'DEMANDABALCAO', 'DEMANDAOFICINA', 'DEMANDATELEMARK', 'OCORRENCIAVENDAS', 'CLASS_ABC', 'CLASS_XYZ', 'TAXA_ESGOTAMENTO'])
-
-    df = df.astype({"EMPRESA": int,
-                    "REVENDA": int,
-                    'ANOMES': int,
-                    'DAATA': str,
-                    'ITEM_ESTOQUE': int,
-                    'ITEM_ESTOQUE_PUB': str,
-                    'DES_ITEM_ESTOQUE': str,
-                    'MARCA': str,
-                    'GRUPO': int,
-                    'CATEGORIA': int,
-                    'DES_CATEGORIA': str,
-                    'IDENTIFICACAO': str,
-                    'UTILIZACAO_ITEM': str,
-                    'DIAS_SEM_MOVIMENTO': int,
-                    'DIAS_MOVIMENTO_INICIAL': int,
-                    'DTA_ULT_ENTRADA': str,
-                    'VAL_ESTOQUE': float,
-                    'QTD_CONTABIL': int,
-                    'CUSTO_MEDIO': float,
-                    'QTD_PEDIDA': int,
-                    'QTD_RES_OFICINA': int,
-                    'QTD_NEGOCIACAO': int,
-                    'QTD_ALOCADA': int,
-                    'QTD_ORCADA': int,
-                    'QTD_LITIGIO': int,
-                    'QTD_TRANSITO': int,
-                    'QTD_TERCEIROS': int,
-                    'ORDEM_TRANSACAO': int,
-                    'BASE_ICMS_RETIDO': float,
-                    'QTD_CONFERENCIA': int,
-                    'QTD_COMPROMETIDA': int,
-                    'PRECO_CUSTO': float,
-                    'PRECO_PUBLICO': float,
-                    'VAL_ESTOQUE_SEM_ICMSRET': float,
-                    'VAL_ICMS_RETIDO': float,
-                    'DEMANDABALCAO': int,
-                    'DEMANDAOFICINA': int,
-                    'DEMANDATELEMARK': int,
-                    'OCORRENCIAVENDAS': int,
-                    'CLASS_ABC': str,
-                    'CLASS_XYZ': int,
-                    'TAXA_ESGOTAMENTO': float})
+    df = pd.DataFrame(results)
+    print(df)
 
     df.to_csv(
-        r'"\\svbi\arquivos\DW\BI Estoque\estoque_2023.csv"', index=False)
+        r"\\svbi\arquivos\DW\BI Estoque\teste_2023.csv", index=False, header=False, sep=',')
 
 
 getEstoque(query=query)
